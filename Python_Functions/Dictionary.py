df73d522d8e4190f80fd52fe50566cad6750b205
Alef Souza <alefsouzadeoliveira@hotmail.com>
2024-11-10 22:30:46 -0300

    added folder
    added new folder

---
+++ Python_Functions/Dictionary.py
@@ -0,0 +1,16 @@
+dictionary_name = {'nome':'Alef','lastname':'oliveira'}
+dictionary_job = {'job':'data professional','languages':['python',  'R']}
+dictionary_languages  = ['Python','R','SQL']
+
+#using variable in dictioinary
+dictionary_alef = {'nome':'alef','language':dictionary_languages}
+
+#print(dictionary_name['lastname'])
+
+
+#print(f'o nome do usuário é : {dictionary_name['nome']}')
+#print(f' a profissao dele é : {dictionary_job['job']}')
+
+
+
+print(dictionary_name)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Python - Development
- Python scrips
